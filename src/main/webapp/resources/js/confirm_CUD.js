2a760aae72bf3499db29725cba35b7a20604d584
maslynem <maslynem@student.21-school.ru>
2023-08-13 13:10:44 +0300

    done data

---
+++ src/main/webapp/resources/js/confirm_CUD.js
@@ -0,0 +1,12 @@
+function confirm_delete() {
+    return confirm("Are you sure you want to do delete a record?");
+}
+
+function confirm_update() {
+    return confirm("Are you sure you want to do update a record?");
+}
+
+function confirm_save() {
+    return confirm("Are you sure you want to do insert a record?");
+}
+
--- src/main/webapp/resources/js/confirm_delete.js
+++
@@ -1,3 +0,0 @@
-function confirm_delete(t) {
-    return confirm("Are you sure you want to do delete a record?");
-}
